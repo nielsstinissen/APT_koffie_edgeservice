8bcd9687a8deabff92fe4b9ac3221e8e82670a1c
nielsstinissen <r0784156@student.thomasmore.be>
2022-01-03 10:48:36 +0100

    Added the models

---
+++ src/main/java/com/example/edgeservice/model/Gerecht.java
@@ -0,0 +1,113 @@
+package com.example.edgeservice.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+public class Gerecht {
+    private String id;
+    private int koffieDrankId;
+    private String naam;
+    private String afkomst;
+    private double kcal;
+    private boolean isGlutenvrij;
+    private boolean isVegan;
+    private boolean isVegetarisch;
+    private int aantalPersonen;
+    private String url;
+
+    public Gerecht(int koffieDrankId, String naam, String afkomst, double kcal, boolean isGlutenvrij, boolean isVegan, boolean isVegetarisch, int aantalPersonen, String url) {
+        this.naam = naam;
+        this.koffieDrankId = koffieDrankId;
+        this.afkomst = afkomst;
+        this.kcal = kcal;
+        this.isGlutenvrij = isGlutenvrij;
+        this.isVegan = isVegan;
+        this.isVegetarisch = isVegetarisch;
+        this.aantalPersonen = aantalPersonen;
+        this.url = url;
+    }
+
+    public Gerecht() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getAfkomst() {
+        return afkomst;
+    }
+
+    public void setAfkomst(String afkomst) {
+        this.afkomst = afkomst;
+    }
+
+    public double getKcal() {
+        return kcal;
+    }
+
+    public void setKcal(double kcal) {
+        this.kcal = kcal;
+    }
+
+    public boolean isGlutenvrij() {
+        return isGlutenvrij;
+    }
+
+    public void setGlutenvrij(boolean isGlutenvrij) {
+        this.isGlutenvrij = isGlutenvrij;
+    }
+
+    public boolean isVegan() {
+        return isVegan;
+    }
+
+    public void setVegan(boolean vegan) {
+        isVegan = vegan;
+    }
+
+    public boolean isVegetarisch() {
+        return isVegetarisch;
+    }
+
+    public void setVegetarisch(boolean vegetarisch) {
+        isVegetarisch = vegetarisch;
+    }
+
+    public int getAantalPersonen() {
+        return aantalPersonen;
+    }
+
+    public void setAantalPersonen(int aantalPersonen) {
+        this.aantalPersonen = aantalPersonen;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getKoffieDrankId() {
+        return koffieDrankId;
+    }
+
+    public void setKoffieDrankId(int koffieDrankId) {
+        this.koffieDrankId = koffieDrankId;
+    }
+}
---
+++ src/main/java/com/example/edgeservice/model/Koffie.java
@@ -0,0 +1,89 @@
+package com.example.edgeservice.model;
+
+import javax.persistence.*;
+
+public class Koffie {
+    private Long id;
+    private String name;
+    private String wheremade;
+    private String whenmade;
+    private String importantsingredient;
+    private String meaning;
+    private String soort;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Koffie() {
+    }
+
+//    public Koffie(String name, String wheremade, String whenmade, String importantsingredient, String meaning, String soort) {
+//        this.name = name;
+//        this.wheremade = wheremade;
+//        this.whenmade = whenmade;
+//        this.importantsingredient = importantsingredient;
+//        this.meaning = meaning;
+//        this.soort = soort;
+//    }
+
+    public Koffie(Long id, String name, String wheremade, String whenmade, String importantsingredient, String meaning, String soort) {
+        this.id = id;
+        this.name = name;
+        this.wheremade = wheremade;
+        this.whenmade = whenmade;
+        this.importantsingredient = importantsingredient;
+        this.meaning = meaning;
+        this.soort = soort;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWhere_made() {
+        return wheremade;
+    }
+
+    public void setWhere_made(String where_made) {
+        this.wheremade = where_made;
+    }
+
+    public String getWhen_made() {
+        return whenmade;
+    }
+
+    public void setWhen_made(String when_made) {
+        this.whenmade = when_made;
+    }
+
+    public String getImportants_ingredient() {
+        return importantsingredient;
+    }
+
+    public void setImportants_ingredient(String importants_ingredient) {this.importantsingredient = importants_ingredient;}
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+
+    public String getSoort() {
+        return soort;
+    }
+
+    public void setSoort(String soort) {
+        this.soort = soort;
+    }
+}
