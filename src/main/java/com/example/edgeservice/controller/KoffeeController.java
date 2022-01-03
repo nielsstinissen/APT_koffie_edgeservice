4625eaf20005396d0cdee4cceb9e686a557e5e50
Arnaud Provoost <57799410+ArnaudProvoost@users.noreply.github.com>
2022-01-03 14:38:28 +0100

    yeet v3

--- src/main/java/com/example/edgeservice/controller/KoffeeController.java
+++ src/main/java/com/example/edgeservice/controller/KoffeeController.java
@@ -15,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "*",maxAge = 3600)
 @RestController
 public class KoffeeController {
 
